90a36ebe96bbd99126bb0ce9c40a58c5a3004c90
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-04-01 00:38:24 -0300

    Removi método AdicionarCarrinho da classe Program. Criei método EscolherClasse na classe PassagemAerea. Inseri Voo no construtor da classe PassagemAerea. E modifiquei os métodos: CalcularValorPassagem e GetResumo da classe PassagemAerea e IniciarSistema da classe Program

--- PassagensAereas/PassagemAerea.cs
+++ PassagensAereas/PassagemAerea.cs
@@ -6,19 +6,19 @@ namespace ProjetoAgenciaDeTurismo.PassagensAereas
         public string Classe { get; private set; }
         public string Poltrona { get; private set; }
         public double ValorDaPassagem { get; private set; }
-        public DateTime HorarioDeEmbarque { get; private set; }
-        public DateTime DataDaPassagem { get; private set; }
+        public TimeOnly HorarioDeEmbarque { get; private set; }
+        public DateOnly DataDaPassagem { get; private set; }
         public Voo Voo { get; private set; }
         public bool MalasParaDespachar { get; private set; }
         public Cliente Cliente { get; private set; }
 
-        public PassagemAerea(string empresaArea, string classe, string poltrona, DateTime horarioDeEmbarque, DateTime dataDaPassagem, Cliente cliente)
+        public PassagemAerea(string empresaArea, string poltrona, TimeOnly horarioDeEmbarque, DateOnly dataDaPassagem, Cliente cliente)
         {
             EmpresaAerea = empresaArea;
-            Classe = classe;
             Poltrona = poltrona;
             HorarioDeEmbarque = horarioDeEmbarque;
             DataDaPassagem = dataDaPassagem;
+            Voo = new Voo(555, HorarioDeEmbarque.AddMinutes(45) , "Fortaleza", "Porto Alegre");
             MalasParaDespachar = false;
             Cliente = cliente;
         }
@@ -30,16 +30,49 @@ namespace ProjetoAgenciaDeTurismo.PassagensAereas
         {
             MalasParaDespachar = false;
         }
-        public void CalcularValorPassagem(){
+        public void EscolherClassePassagem()
+        {
+            bool escolhaFeita = false;
+            while (escolhaFeita == false)
+            {
+                Console.WriteLine("Qual a classe para sua passagem? 1-Econômica | 2-Primeira Classe");
+                string escolha = Console.ReadLine();
+                if (escolha == "1")
+                {
+                    Console.WriteLine("Classe escolhida: Econômica");
+                    Classe = "Econômica";
+                    escolhaFeita = true;
+                }
+                else if (escolha == "2")
+                {
+                    Console.WriteLine("Opção inválida!");
+                    Classe = "Primeira Classe";
+                    escolhaFeita = true;
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida!");
+                }
+            }
+        }
+        public void CalcularValorPassagem()
+        {
             double valorBase = 500;
-            valorBase = valorBase + (Voo.EscalasDoVoo.Count()*100);
-            if(MalasParaDespachar == true){
+            valorBase = valorBase + (Voo.EscalasDoVoo.Count() * 100);
+            if (Classe == "Primeira Classe")
+            {
+                valorBase = valorBase * 2;
+            }
+            if (MalasParaDespachar == true)
+            {
                 valorBase = valorBase + 150;
             }
             ValorDaPassagem = valorBase;
         }
-        public void GetResumo(){
-            Console.WriteLine($"Resumo da passagem: Nome: {Cliente.RetornaPrimeiroNome()}, Poltrona: {Poltrona} e Valor: {ValorDaPassagem}");
+        public void GetResumo()
+        {
+            Console.WriteLine($"Resumo da passagem: Nome: {Cliente.RetornaPrimeiroNome()}, Poltrona: {Poltrona}, Classe: {Classe} e Valor: {ValorDaPassagem}");
+            Console.WriteLine($"Saindo de {Voo.OrigemDoVoo}, para {Voo.DestinoDoVoo}. Este voo tem {Voo.EscalasDoVoo.Count()} escalas.");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using ProjetoAgenciaDeTurismo.PassagensAereas;
 namespace ProjetoAgenciaDeTurismo{
     public class Program{
         static void Main(string[] args){
-
+            IniciarSistema();
         }
         public static Cliente CadastrarCliente(){
             Console.WriteLine("Para começar, vamos fazer seu cadastro e liberar seu carrinho de compras.");
@@ -18,14 +18,30 @@ namespace ProjetoAgenciaDeTurismo{
             cliente.VerificaSeNomeÉValido();
             return cliente;
         }
-        public static void CriarCarrinho(){
-            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
-        }
+
         public static void IniciarSistema(){
             Console.WriteLine("Seja bem vindo a DoDev Turismo!");
+            var cliente = CadastrarCliente();
+            var carrinhoDeCompras = new CarrinhoDeCompras();
+            carrinhoDeCompras.AdicionarCliente(cliente);
+            string escolha;
             do{
-                Console.WriteLine("Qual produto deseja? 1-Pacote de viagem | 2-Passagem Aérea");
-            } while();
+                Console.WriteLine("O que deseja fazer? 1-Continuar Comprando | 2-Finalizar Compra");
+                escolha = Console.ReadLine();
+                switch(escolha){
+                    case "1":
+                    carrinhoDeCompras.MenuDeComprasCarrinho();
+                    break;
+                    case "2":
+                    carrinhoDeCompras.EscolherFormaDePagamento();
+                    carrinhoDeCompras.FinalizarCompra();
+                    Console.WriteLine("Obrigado por escolher nossa agência! ^-^");
+                    break;
+                    default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+                }
+            } while(escolha != "2");
         }
     }
 }
